28f037303a91ea0be9a74442bd451b0eebdfc11e
Sunday Onwuchekwa <donsonde@gmail.com>
2019-10-04 03:54:33 +0000

    update1_03102019

--- lesson3/js/myScript.js
+++ lesson3/js/myScript.js
@@ -1,2 +1,15 @@
 //Function to get current year
 document.getElementById('current-year').innerHTML = new Date().getFullYear();
+
+var accordion = document.getElementsByClassName('accordion');
+for(var i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle("accordion-active");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
