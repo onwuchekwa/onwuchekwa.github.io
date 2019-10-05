8d7ab3e8a82a2f814d83bd3d7b484f0aa04046ad
Sunday Onwuchekwa <donsonde@gmail.com>
2019-10-05 08:07:05 +0000

    update_05102019

--- lesson3/js/myScript.js
+++ lesson3/js/myScript.js
@@ -1,5 +1,7 @@
 //Function to get current year
 document.getElementById('current-year').innerHTML = new Date().getFullYear();
+//Get document last modified date
+document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
 
 var accordion = document.getElementsByClassName('accordion');
 for(var i = 0; i < accordion.length; i++) {
