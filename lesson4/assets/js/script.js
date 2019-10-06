b3554f5794af3bc1f1021305b6c57ba5bd1755ee
donsonde@gmail.com <donsonde@gmail.com>
2019-10-06 18:52:11 +0000

    update-3-06102019

---
+++ lesson4/assets/js/script.js
@@ -0,0 +1,4 @@
+//Function to get current year
+document.getElementById('current-year').innerHTML = new Date().getFullYear();
+//Get document last modified date
+document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
