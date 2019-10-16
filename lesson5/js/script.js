b7fd428641dad0cc36d85777a51a2eb39910c298
donsonde@gmail.com <donsonde@gmail.com>
2019-10-16 02:40:35 +0000

    update-6-15102019

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -6,7 +6,7 @@ function toggleMenu() {
     document.getElementsByClassName("main-nav")[0].classList.toggle("responsive");  
 }
 
-//Footer Date function
+//Date function
 function getFullDate() {
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -23,4 +23,25 @@ function getFullDate() {
 }
 
 var dateInfo = getFullDate();
-document.getElementById('todayDate').innerHTML = dateInfo.fullDate; //dateInfo.weekDayName;
+
+//Footer Date
+document.getElementById('todayDate').innerHTML = dateInfo.fullDate;
+
+window.onresize = function () {
+    //Friday Banner
+    var friday_banner = document.getElementById('friday-banner');
+    var main_header = document.getElementById('main-header');
+    var device_width = window.innerWidth;
+    if (dateInfo.weekDayName == 'Friday') {
+        friday_banner.style.display = "block";
+        if (device_width < 320) {
+            main_header.style.paddingTop = '90px';
+        } else if (device_width < 768) {
+            main_header.style.paddingTop = '70px';
+        } else if (device_width < 960) {
+            main_header.style.paddingTop = '50px';
+        } else {
+            main_header.style.paddingTop = '40px';
+        }
+    }
+}
