99bb2d01ecabe45301f5beb4f75162e81882a0fc
donsonde@gmail.com <donsonde@gmail.com>
2019-11-23 20:58:48 +0000

    update-1-23112019

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -120,3 +120,32 @@ fetch(requestURL)
       }
     }
   });
+
+  //Get Town Event
+  fetch(requestURL)
+  .then(function(responseEvent) {
+    return responseEvent.json();
+  })
+  .then(function(townJsonEvent) {
+    console.table(townJsonEvent);
+
+    const eventObj = townJsonEvent['towns'];
+    const getTownEventName = document.getElementById('preston-town-header');
+    const getTownEventImage = document.getElementById('preston-event-image');
+    for(let i = 0; i < eventObj.length; i++) {
+      if (eventObj[i].name == 'Preston'){
+        getTownEventName.innerHTML = eventObj[i].name + '&#39;s Upcoming Event:';
+        for (let j = 0; j < eventObj[i].events.length; j++){
+          const pEvent = document.createElement('p');
+          pEvent.setAttribute('class', 'para-event');
+          pEvent.textContent = eventObj[i].events[j];
+          document.querySelector('div.event-panel').appendChild(pEvent);
+        }
+        const eventImage = document.createElement('img');
+        eventImage.setAttribute('class', 'img1');
+        eventImage.setAttribute('src', 'images/preston-event.jpg');
+        eventImage.setAttribute('alt', eventObj[i].name + 'events');
+        getTownEventImage.appendChild(eventImage);
+      }
+    }
+  });
